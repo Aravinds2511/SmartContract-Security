ec24a7952307197f4210c4f9ab3ad6076b1c0c16
Aravinds2511 <aravindvasang@gmail.com>
2025-03-31 15:57:48 +0530

    feat: thunder loan audit

--- t-swap-audit/script/DeployTSwap.t.sol
+++ t-swap-audit/script/DeployTSwap.t.sol
@@ -1,13 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-import { Script } from "forge-std/Script.sol";
-import { PoolFactory } from "../src/PoolFactory.sol";
-import { TSwapPool } from "../src/TSwapPool.sol";
-import { ERC20Mock } from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
+import {Script} from "forge-std/Script.sol";
+import {PoolFactory} from "../src/PoolFactory.sol";
+import {TSwapPool} from "../src/TSwapPool.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
 
 contract DeployTSwap is Script {
-    address public constant WETH_TOKEN_MAINNET = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address public constant WETH_TOKEN_MAINNET =
+        0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
     uint256 public constant MAINNET_CHAIN_ID = 1;
 
     function run() public {
